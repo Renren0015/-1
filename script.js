e8aa7b2018554292aadb1ccb60f40a863a4289ed
Renren0015 <57824147+Renren0015@users.noreply.github.com>
2020-01-22 15:56:09 +0800

    Problem #1

---
+++ script.js
@@ -0,0 +1,20 @@
+function add() {
+    var x = parseInt(prompt('Input number', 'Input your number'));
+    var y = parseInt(prompt('Input number', 'Input your another number'));
+    var z = alert(x + y);
+}
+function sub() {
+    var x = parseInt(prompt('Input number', 'Input your number'));
+    var y = parseInt(prompt('Input number', 'Input your another number'));
+    var z = alert(x - y);
+}
+function mult() {
+    var x = parseInt(prompt('Input number', 'Input your number'));
+    var y = parseInt(prompt('Input number', 'Input your another number'));
+    var z = alert(x * y);
+}
+function div() {
+    var x = parseInt(prompt('Input number', 'Input your number'));
+    var y = parseInt(prompt('Input number', 'Input your another number'));
+    var z = alert(x / y);
+}
